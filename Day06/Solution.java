23aead5d304f8387414f0097447af88ca98b20c7
Sudhanshu <2201030100@cgu-odisha.ac.in>
2024-06-14 20:33:22 +0530

    Solving String leetcode problem lc 415

---
+++ Day06/Solution.java
@@ -0,0 +1,22 @@
+package Suku;
+
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int i = num1.length() - 1, j = num2.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int x = (i >= 0) ? num1.charAt(i) - '0' : 0;
+            int y = (j >= 0) ? num2.charAt(j) - '0' : 0;
+            int sum = x + y + carry;
+            result.append(sum % 10);
+            carry = sum / 10;
+            i--;
+            j--;
+        }
+
+        return result.reverse().toString();
+    }
+}
+
