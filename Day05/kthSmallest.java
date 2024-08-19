7758819b11c648dde5d4fca9dc93dc7acf3a6536
Sudhanshu <125084256+Sudhaanshuu@users.noreply.github.com>
2024-08-19 18:48:26 +0530

    Create kthSmallest.java

---
+++ Day05/kthSmallest.java
@@ -0,0 +1,12 @@
+class Solution {
+    public static int kthSmallest(int[] arr, int k) {
+        // Your code here
+        int n = arr.length;
+        Arrays.sort(arr);
+        for(int num:arr){
+            
+        }
+        return(arr[k-1]);
+
+    }
+}
